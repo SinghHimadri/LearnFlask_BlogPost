a705948a684bc785497a7771e416d93e5d6705e8
SinghHimadri <himsingh1612@gmail.com>
2020-07-21 11:30:30 +0530

    template static done

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///posts.db'
+db= SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id=
 
 all_posts= [
     {
