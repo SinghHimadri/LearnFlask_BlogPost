e84b7a80560c2d324e4add8ef44bc4f65f48006b
SinghHimadri <himsingh1612@gmail.com>
2020-07-25 23:53:20 +0530

    edit option added backend completed

--- app.py
+++ app.py
@@ -43,5 +43,19 @@ def delete(id):
     db.session.commit()
     return redirect('/posts')
 
+@app.route('/posts/edit/<int:id>', methods=['GET','POST'])
+def edit(id):
+
+    post = BlogPost.query.get_or_404(id)
+    
+    if request.method == 'POST': 
+        post.title = request.form['title']
+        post.content = request.form['content']
+        post.author = request.form['author']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post = post)
+
 if __name__ == "__main__":
     app.run(debug=True)   
