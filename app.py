bf4d63169077f8e5102d86df7a49219bce3fe55e
SinghHimadri <himsingh1612@gmail.com>
2020-07-25 23:25:21 +0530

    delete post feaature added

--- app.py
+++ app.py
@@ -36,6 +36,12 @@ def posts():
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
 
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
 
 if __name__ == "__main__":
     app.run(debug=True)   
